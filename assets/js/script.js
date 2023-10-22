d40a307ad063a0a66f59acacc9562c08b24e82d9
vlad <vkakachko@mail.ru>
2023-10-22 11:19:49 +0300

    fisrt-commit

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".mob-burger").addEventListener("click", () => {
+    document.querySelector(".mob-burger").classList.toggle("active")
+    document.querySelector(".header-mob-menu").classList.toggle("active")
+})
