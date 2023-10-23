866f81234a70326d1c6fea46281046580ae0511a
vlad <vkakachko@mail.ru>
2023-10-23 20:48:35 +0300

    header & footer & slider changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,44 @@ document.querySelector(".mob-burger").addEventListener("click", () => {
     document.querySelector(".mob-burger").classList.toggle("active")
     document.querySelector(".header-mob-menu").classList.toggle("active")
 })
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    speed: 500,
+    effect: 'fade',
+    grabCursor: true,
+    breakpoints: {
+        992: {
+            direction: 'vertical',
+        },
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
+const swiperNews = new Swiper('.swiper-news', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: auto,
+    speed: 500,
+    effect: 'fade',
+    grabCursor: true,
+    pagination: {
+        el: '.news-pag',
+    },
+    navigation: {
+        nextEl: '.news-prev',
+        prevEl: '.news-next',
+    },
+
+});
